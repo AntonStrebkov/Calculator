f922a3406661bfef850c8ced9c190d9364714a09
AntonStrebkov <wancler987@gmail.com>
2023-01-17 22:41:42 +0700

    initial commit

---
+++ Calculator/src/MultiplyModel.java
@@ -0,0 +1,31 @@
+public class MultiplyModel extends CalcModel{
+    @Override
+    public Complex result() {
+        return null;
+    }
+
+    @Override
+    public Rational result1() {
+        return null;
+    }
+
+    @Override
+    public void setA(Complex c) {
+
+    }
+
+    @Override
+    public void setRatioA(Rational c) {
+
+    }
+
+    @Override
+    public void setB(Complex c) {
+
+    }
+
+    @Override
+    public void setRatioB(Rational c) {
+
+    }
+}
